f181ad509c2dd9115ae7a8e92c653e79570e0f43
Aryan Rajeev <raghavaan27@gmail.com>
2025-02-26 15:36:45 +0530

    Added menu for users to enter their choice of operation to be performed

--- inventorySmart.cpp
+++ inventorySmart.cpp
@@ -1,8 +1,49 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+int menu(){
+    int choice;
+    cout << "------------------------------" << endl;
+    cout << "Enter 0 to exit" << endl;
+    cout << "Enter 1 to add products" << endl;
+    cout << "Enter 2 to display products" << endl;
+    cout << "------------------------------" << endl;
+    cout << "Enter your choice" << endl;
+    cin >> choice;
+    return choice;
+}
 
-int main(){
-    cout << "Hello!";
+class Product{
+private:
+    int product_id, quantity;
+    char product_name[50];
+
+public:
+    // Add Product
+    // Remove Product
+    // Display Product
+};
+
+int main()
+{
+    int choice;
+    while ((choice = menu()) != 0)
+    {
+        switch (choice)
+        {
+        case 1:
+            cout << "Add products here"<<endl;
+            break;
+
+        case 2:
+            cout << "Display products here"<<endl;
+            break;
+
+        default:
+            cout << "Enter a valid choice" << endl;
+            break;
+        }
+    }
     return 0;
 }
